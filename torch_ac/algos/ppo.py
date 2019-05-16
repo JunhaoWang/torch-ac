correct KL adding

torch_ac/algos/ppo.py
@@ -270,10 +270,10 @@ class PPOAlgo(BaseAlgo):
 
                     ratio = torch.exp(dist.log_prob(sb.action) - sb.log_prob)
 
-                    surr1 = ratio
-                    surr2 = torch.clamp(ratio, 1.0 - self.clip_eps, 1.0 + self.clip_eps)
+                    surr1 = ratio * sb.advantage
+                    surr2 = torch.clamp(ratio, 1.0 - self.clip_eps, 1.0 + self.clip_eps) * sb.advantage
 
-                    policy_loss = (-torch.min(surr1, surr2) * sb.advantage).mean()
+                    policy_loss = (-torch.min(surr1, surr2)).mean()
 
 
                     value_clipped = sb.value + torch.clamp(value - sb.value, -self.clip_eps, self.clip_eps)